284b560d3d70bedf6c3c0752eaf7630d48fd7b85
Adrian <alextravc.342@gmail.com>
2023-10-17 20:57:05 -0500

    [ADD]: MovieRepositoryImpl (getNowPlaying)

---
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,18 @@
+import 'package:cinema_pedia/domain/datasources/movies_datasource.dart';
+import 'package:cinema_pedia/domain/entities/movie.dart';
+import 'package:cinema_pedia/domain/repositories/movies_repository.dart';
+
+
+class MovieRepositoryImpl extends MoviesRepository {
+  final MoviesDataSource moviesDataSource;
+
+
+  MovieRepositoryImpl(this.moviesDataSource);
+
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return moviesDataSource.getNowPlaying();
+  }
+
+}
