778151df06dc069a21279d5bf5efe917fe7eaabb
Adrian <alextravc.342@gmail.com>
2023-10-18 09:00:45 -0500

    [ADD]: CustomAppbar initial design

---
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+
+class CustomAppbar extends StatelessWidget {
+  const CustomAppbar({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme;
+    final titleStyle = Theme.of(context).textTheme.titleMedium;
+
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+
+        child: SizedBox(
+          width: double.infinity, // todo el width q se pueda
+
+          child: Row(
+            children: [
+              Icon(Icons.movie_outlined, color: color.primary),
+              const SizedBox(width: 5),
+              Text('Cinemapedia', style: titleStyle),
+
+              const Spacer(), // like flex 1
+              IconButton(
+                onPressed: (){},
+                icon: const Icon(Icons.search)
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
