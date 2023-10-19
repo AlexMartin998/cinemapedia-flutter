a100e888139b6016135f90428815b8199496ad94
Adrian <alextravc.342@gmail.com>
2023-10-19 16:19:33 -0500

    [ADD]: movie screen | some animations

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:cinema_pedia/domain/entities/movie.dart';
 import 'package:cinema_pedia/presentation/providers/movies/movie_info_provider.dart';
 import 'package:cinema_pedia/presentation/providers/providers.dart';
@@ -103,10 +104,17 @@ class _CustomSliverAppBar extends StatelessWidget {
 
         background: Stack( // 1 sobre otro
           children: [
+            /* image */
             SizedBox.expand(
               child: Image.network(
                 movie.posterPath,
                 fit: BoxFit.cover,
+
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) return FadeIn(child: child);
+
+                  return const SizedBox();
+                },
               ),
             ),
 
@@ -265,13 +273,15 @@ class _ActorsByMovie extends ConsumerWidget {
 
               children: [
                 /* image */
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image.network(
-                    actor.profilePath,
-                    height: 180,
-                    width: 135,
-                    fit: BoxFit.cover,
+                FadeInRight(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      actor.profilePath,
+                      height: 180,
+                      width: 135,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
                 const SizedBox(height: 5),
