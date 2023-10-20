b79e217b28021230e750a6daaab4213301117f52
Adrian <alextravc.342@gmail.com>
2023-10-20 14:05:02 -0500

    [ADD]: ShellRoute

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,5 +1,7 @@
-import 'package:cinema_pedia/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
+
+import 'package:cinema_pedia/presentation/views/views.dart';
+import 'package:cinema_pedia/presentation/screens/screens.dart';
 
 
 // facilita el W. Da compatibilidad web con deeplinking
@@ -7,10 +9,50 @@ final appRouter = GoRouter(
   initialLocation: '/',
 
   routes: [
+    // // Work with BottomNavigationBar
+    ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
+
+      // rutas
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) {
+            return const HomeView();
+          },
+
+          // child route
+          routes: [
+            GoRoute(
+              path: 'movie/:id', // always string
+              name: MovieScreen.name,
+
+              builder: (context, state) {
+                final movieId = state.pathParameters['id'] ?? 'no-id';
+
+                return MovieScreen(movieId: movieId);
+              },
+            ),
+          ]
+        ),
+
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) => const FavoritesView(),
+        )
+      ]
+    ),
+
+
+    /* ** Rutas normales: Padre/Hijo
     GoRoute(
       path: '/',
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state) => const HomeScreen(
+        childView: HomeView(),
+      ),
 
       // routes child
       routes: [
@@ -26,7 +68,7 @@ final appRouter = GoRouter(
         ),
       ]
     ),
-
+    */
 
   ]
 );
