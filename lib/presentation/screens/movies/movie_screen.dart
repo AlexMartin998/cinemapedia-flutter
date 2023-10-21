1b3fc63f112e40529ba5598e1194f71a496ea885
Adrian <alextravc.342@gmail.com>
2023-10-20 22:51:22 -0500

    [ADD]: _CustomGradient

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -85,6 +85,15 @@ class _CustomSliverAppBar extends StatelessWidget {
       expandedHeight: size.height * 0.7,
       foregroundColor: Colors.white,
 
+      // I Like it Btn
+      actions: [
+        IconButton(
+          onPressed: (){}, 
+          icon: const Icon(Icons.favorite_border),
+          // icon: const Icon(Icons.favorite_rounded, color: Colors.red),
+        ),
+      ],
+
       // same goBack btn
       leading: IconButton(
         onPressed: (){context.pop();}, // pop() <- go_router
@@ -119,36 +128,34 @@ class _CustomSliverAppBar extends StatelessWidget {
             ),
 
             /* gradient */
-            const SizedBox.expand( // take all space
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    // de arriba abajo
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    stops: [0.7, 1.0],
-                    colors: [
-                      Colors.transparent,
-                      Colors.black87
-                    ]
-                  ),
-                ),
-              ),
+            const _CustomGradient(
+              // de arriba abajo
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.75, 1.0],
+              colors: [
+                Colors.transparent,
+                Colors.black87
+              ]
             ),
-            const SizedBox.expand( // take all space
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    stops: [0.0, 0.3],
-                    colors: [
-                      Colors.black87,
-                      Colors.transparent,
-                    ]
-                  ),
-                ),
-              ),
+            const _CustomGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              stops: [0.0, 0.2],
+              colors: [
+                Colors.black54,
+                Colors.transparent,
+              ]
             ),
+            const _CustomGradient(
+              begin: Alignment.topLeft,
+              stops: [0.0, 0.3],
+              colors: [
+                Colors.black87,
+                Colors.transparent,
+              ]
+            ),
+          
           ],
         ),
         
@@ -300,6 +307,39 @@ class _ActorsByMovie extends ConsumerWidget {
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+
+
+class _CustomGradient extends StatelessWidget {
+  final AlignmentGeometry begin;
+  final AlignmentGeometry end;
+  final List<double> stops;
+  final List<Color> colors;
+
+
+  const _CustomGradient({
+    this.begin = Alignment.centerLeft,
+    this.end = Alignment.centerRight,
+    required this.stops,
+    required this.colors,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand( // take all space
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: begin,
+            end: end,
+            stops: stops,
+            colors: colors,
+          ),
+        ),
       ),
     );
   }
