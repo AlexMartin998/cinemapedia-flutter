f51e270b0bdd867c74970eba4c9edc7910fe3cc4
Adrian <alextravc.342@gmail.com>
2023-10-21 12:42:42 -0500

    [ADD]: MovieMasonry initial design

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,18 +1,34 @@
+import 'package:cinema_pedia/presentation/providers/providers.dart';
+import 'package:cinema_pedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final favoriteMoviesMap = ref.watch(favoriteMoviesProvider);
+    final favoriteMoviesList = favoriteMoviesMap.values.toList();
+
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Favorite View'),
-      ),
-
-      body: Center(child: Text('Favorites')),
+      body: MovieMasonry(movies: favoriteMoviesList),
     );
   }
 }
