37644b4d0f0cebd1cd9df004de5f72fb68238ec6
Adrian <alextravc.342@gmail.com>
2023-10-22 09:13:50 -0500

    [ADD]: _Recomendations in order to handle empy recomendation

--- lib/presentation/widgets/movies/similar_movies.dart
+++ lib/presentation/widgets/movies/similar_movies.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:cinema_pedia/domain/entities/movie.dart';
 import 'package:cinema_pedia/presentation/providers/movies/movies_repository_provider.dart';
 import 'package:cinema_pedia/presentation/widgets/movies/movie_horizontal_listview.dart';
+
 
 
 // // // lo crea aqui x simplicidad ya q solo se va a usar aqui este provider
@@ -28,18 +30,34 @@ class SimilarMovies extends ConsumerWidget {
 
     // Performs an action based on the state of the AsyncValue<> (List<Movie>)
     return similarMoviesFuture.when(
-      data: (movies) => Container(
-        margin: const EdgeInsetsDirectional.only(bottom: 12),
-        child: MovieHorizontalListview(
-          title: 'Recomendaciones',
-          movies: movies,
-        ),
-      ),
+      data: (movies) => _Recomendations(movies: movies),
 
       error: (_ , __) => const Center(child: Text('No se pudo cargar películas similares') ), 
       loading: () => const Center(child: CircularProgressIndicator(strokeWidth: 2)),
     );
 
   }
+}
 
+
+
+class _Recomendations extends StatelessWidget {
+  final List<Movie> movies;
+
+  const _Recomendations({ required this.movies });
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    if ( movies.isEmpty ) return const SizedBox();
+
+    return Container(
+      margin: const EdgeInsetsDirectional.only(bottom: 50),
+      child: MovieHorizontalListview(
+        title: 'Recomendaciones',
+        movies: movies
+      ),
+    );
+  }
 }
